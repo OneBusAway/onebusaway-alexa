49bfbb60641d7f33ca9644ae0190ccb24d0aed2d
Philip White <philip@mailworks.org>
2016-02-21 20:28:58 -0800

    Use JMockit; add unit tests

---
+++ src/test/java/org/onebusaway/alexa/AuthedSpeechletTest.java
@@ -0,0 +1,76 @@
+package org.onebusaway.alexa;
+
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import config.UnitTests;
+import mockit.Expectations;
+import mockit.Mocked;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onebusaway.alexa.lib.ObaUserClient;
+import org.onebusaway.alexa.storage.ObaUserDataItem;
+import org.onebusaway.io.client.elements.ObaArrivalInfo;
+import org.onebusaway.io.client.request.ObaArrivalInfoResponse;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import javax.annotation.Resource;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class,
+        classes = UnitTests.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AuthedSpeechletTest {
+    static final LaunchRequest launchRequest = LaunchRequest.builder().withRequestId("test-req-id").build();
+
+    @Mocked
+    ObaArrivalInfo obaArrivalInfo;
+
+    @Mocked
+    ObaArrivalInfoResponse obaArrivalInfoResponse;
+
+    @Mocked
+    ObaUserClient obaUserClient;
+
+    @Resource
+    Session session;
+
+    @Resource
+    AuthedSpeechlet authedSpeechlet;
+
+    @Resource
+    ObaUserDataItem testUserData;
+
+    @Before
+    public void initializeAuthedSpeechlet() {
+        authedSpeechlet.setUserData(testUserData);
+    }
+
+    @Test
+    public void launchTellsArrivals() throws SpeechletException {
+        ObaArrivalInfo[] obaArrivalInfoArray = new ObaArrivalInfo[1];
+        obaArrivalInfoArray[0] = obaArrivalInfo;
+        new Expectations() {{
+            obaArrivalInfo.getShortName(); result = "8";
+            obaArrivalInfo.getHeadsign(); result = "Mlk Way Jr";
+            obaArrivalInfoResponse.getArrivalInfo(); result = obaArrivalInfoArray;
+            obaUserClient.getArrivalsAndDeparturesForStop(anyString); result = obaArrivalInfoResponse;
+        }};
+
+        SpeechletResponse sr = authedSpeechlet.onLaunch(
+                launchRequest,
+                session);
+
+        String spoken = ((PlainTextOutputSpeech)sr.getOutputSpeech()).getText();
+        assertThat(spoken, equalTo("Route 8 Mlk Way Jr is now arriving based on the schedule -- "));
+    }
+}
--- src/test/java/org/onebusaway/alexa/MainSpeechletEmptyTest.java
+++ src/test/java/org/onebusaway/alexa/MainSpeechletEmptyTest.java
@@ -5,11 +5,18 @@ import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.*;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import config.UnitTests;
-import org.junit.Before;
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.onebusaway.alexa.lib.GoogleMaps;
 import org.onebusaway.alexa.storage.ObaDao;
+import org.onebusaway.alexa.storage.ObaUserDataItem;
+import org.onebusaway.location.Location;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -24,25 +31,40 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class,
                       classes = UnitTests.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MainSpeechletEmptyTest {
     static final LaunchRequest launchRequest = LaunchRequest.builder().withRequestId("test-req-id").build();
-    static final Session session = Session.builder().withSessionId("test-session-id").build();
+
+    @BeforeClass
+    public static void setUpMocksForSpringConfiguration() {
+        new MockUp<GoogleMaps>() {
+            @Mock
+            Optional<Location> geocode(String cityName) {
+                return Optional.of(new Location("test"));
+            }
+        };
+        new MockUp<ObaDao>() {
+            @Mock
+            Optional<ObaUserDataItem> getUserData(Session s) {
+                return Optional.empty();
+            }
+        };
+    }
+
+    @Mocked
+    GoogleMaps googleMaps;
+
+    @Mocked
+    ObaDao obaDao;
 
     @Resource
-    ObaDao obaDao;
+    Session session;
 
     @Resource
     MainSpeechlet mainSpeechlet;
 
-    @Before
-    public void setUpDao() {
-        Mockito.reset(obaDao);
-        Mockito.when(obaDao.getUserData(session)).thenReturn(Optional.empty());
-    }
-
     @Test
     public void launchAsksForCity() throws SpeechletException {
-        System.err.println("launchAsksForCity has " + mainSpeechlet + " for mainSpeechlet");
         SpeechletResponse sr = mainSpeechlet.onLaunch(
                 launchRequest,
                 session);
@@ -70,5 +92,52 @@ public class MainSpeechletEmptyTest {
         assertThat(sr.getOutputSpeech(), is(instanceOf(PlainTextOutputSpeech.class)));
         String spoken = ((PlainTextOutputSpeech)sr.getOutputSpeech()).getText();
         assertThat(spoken, containsString("You have not yet told me where you live."));
+    }
+
+    @Test
+    public void setRecognizableCity() throws SpeechletException {
+        new Expectations() {{
+            googleMaps.geocode("Seattle"); result = Optional.of(new Location("test provider"));
+        }};
+        HashMap<String, Slot> slots = new HashMap<>();
+        slots.put("cityName", Slot.builder()
+                                  .withName("cityName")
+                                  .withValue("Seattle").build());
+        SpeechletResponse sr = mainSpeechlet.onIntent(
+                IntentRequest.builder()
+                .withRequestId("test-request-id")
+                .withIntent(
+                        Intent.builder()
+                        .withName("SetCityIntent")
+                        .withSlots(slots)
+                        .build()
+                )
+                .build(),
+                session
+        );
+        String spoken = ((PlainTextOutputSpeech)sr.getOutputSpeech()).getText();
+        assertThat(spoken, startsWith("Ok, you live in Seattle.  What's your stop number?"));
+    }
+
+    @Test
+    public void setStopBeforeCity() throws SpeechletException {
+        HashMap<String, Slot> slots = new HashMap<>();
+        slots.put("stopNumber", Slot.builder()
+                .withName("stopNumber")
+                .withValue("2245").build());
+        SpeechletResponse sr = mainSpeechlet.onIntent(
+                IntentRequest.builder()
+                        .withRequestId("test-request-id")
+                        .withIntent(
+                                Intent.builder()
+                                        .withName("SetStopNumberIntent")
+                                        .withSlots(slots)
+                                        .build()
+                        )
+                        .build(),
+                session
+        );
+        String spoken = ((PlainTextOutputSpeech)sr.getOutputSpeech()).getText();
+        assertThat(spoken, startsWith("Welcome to OneBusAway! Let's set you up."));
     }
 }
